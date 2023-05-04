9139f993fd5e782da6a01ca854a03c643902c2e4
MarkLambson <mj.lambson@yahoo.com>
2023-05-04 13:38:32 -0700

    conditional rendering

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,11 +2,57 @@ import React, { useState } from 'react'
 
 const Form = () => {
     const [name, setName] = useState("")
+    const [nameError, setNameError] = useState("");
     const [type, setType] = useState("")
+    const [typeError, setTypeError] = useState("");
     const [age, setAge] = useState("")
+    const [ageError, setAgeError] = useState("");
     const [image, setImage] = useState("")
+    const [imageError, setImageError] = useState("");
 
     let [listOfPets, setListOfPets] = useState([])
+
+    const handleName = (event) => {
+        setName(event.target.value);
+        if (event.target.value.length < 1) {
+            setNameError("Name is required!");
+        } else if (event.target.value.length < 2) {
+            setNameError("Name must be 2 characters or longer!");
+        } else {
+            setNameError("");
+        }
+    }
+
+    const handleType = (event) => {
+        setType(event.target.value);
+        if (event.target.value.length < 1) {
+            setTypeError("Type is required!");
+        } else if (event.target.value.length < 2) {
+            setTypeError("Type must be 2 characters or longer!");
+        } else {
+            setTypeError("");
+        }
+    }
+
+    const handleAge = (event) => {
+        setAge(event.target.value);
+        if (event.target.value.length < 1) {
+            setAgeError("Age is required!");
+        } else {
+            setAgeError("");
+        }
+    }
+
+    const handleImage = (event) => {
+        setImage(event.target.value);
+        if (event.target.value.length < 1) {
+            setImageError("Image is required!");
+        } else if (event.target.value.length < 3) {
+            setImageError("Image must be 3 characters or longer!");
+        } else {
+            setImageError("");
+        }
+    }
 
     const submitHandler = (event) => {
         event.preventDefault()
@@ -22,19 +68,27 @@ const Form = () => {
             <form onSubmit={submitHandler}>
                 <div className="form-group">
                     <label>Pet Name:</label>
-                    <input onChange={(event) => setName(event.target.value)} type="text" className='form-label' />
+                    {/* <input onChange={(event) => setName(event.target.value)} type="text" className='form-label' /> */}
+                    <input onChange={handleName} type="text" className='form-label' />
+                    {nameError ? <p>{nameError}</p> : ''}
                 </div>
                 <div className="form-group">
                     <label>Pet Type:</label>
-                    <input onChange={(event) => setType(event.target.value)} type="text" className='form-label' />
+                    {/* <input onChange={(event) => setType(event.target.value)} type="text" className='form-label' /> */}
+                    <input onChange={handleType} type="text" className='form-label' />
+                    {typeError ? <p>{typeError}</p> : ''}
                 </div>
                 <div className="form-group">
                     <label>Pet Age:</label>
-                    <input onChange={(event) => setAge(event.target.value)} type="text" className='form-label' />
+                    {/* <input onChange={(event) => setAge(event.target.value)} type="text" className='form-label' /> */}
+                    <input onChange={handleAge} type="text" className='form-label' />
+                    {ageError ? <p>{ageError}</p> : ''}
                 </div>
                 <div className="form-group">
                     <label>Pet Image:</label>
-                    <input onChange={(event) => setImage(event.target.value)} type="text" className='form-label' />
+                    {/* <input onChange={(event) => setImage(event.target.value)} type="text" className='form-label' /> */}
+                    <input onChange={handleImage} type="text" className='form-label' />
+                    {imageError ? <p>{imageError}</p> : ''}
                 </div>
                 <button className='btn btn-outline-success mt-3' type='submit'>Sumbit Pet</button>
             </form>
